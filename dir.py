1ae00b8247c36c8028d10d82fe3375c0d1a70671
Audrey <aliv074@aucklanduni.ac.nz>
2019-12-02 18:50:42 +1300

    Created helper for renaming torrented video files/deleting subtitle files

---
+++ dir.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import os
+import re
+
+class DirectoryActivities:
+    def __init__(self):
+        self.chdir = os.getcwd() + "/"
+
+    def cleanSrtFiles(self):
+        for file in os.listdir(self.chdir):
+            if file.endswith(".srt"):
+                os.remove(file)
+
+    def renameVideos(self):
+        for file in os.listdir(self.chdir):
+            if file.endswith(".mkv"):
+                dst = self.checkPatterns(file)
+                src = self.chdir + file
+                dst = self.chdir + dst + ".mkv"
+
+                os.rename(src, dst)
+
+    def checkPatterns(self, check):
+        patterns = ["S\d{1,2}E\d{1,2}", "\dx\d{1,2}"]
+        for i in patterns:
+            match = re.findall(i, check)
+            if len(match) != 0:
+                return match[0]
+
+    def vidCleanUp(self):
+        self.cleanSrtFiles()
+        self.renameVideos()
+
+if __name__ == "__main__":
+    dir = DirectoryActivities()
+    dir.vidCleanUp()
